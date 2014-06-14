97b2f631f79d2d94e8e887275dbe960fe63a2081
2kblater.com <2kblater.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-14 18:02:07 +0000

    version up
    git-svn-id: https://plugins.svn.wordpress.org/2kb-amazon-affiliates-store/trunk@932409 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: 2kb Amazon Affiliates Store
  * Plugin URI: http://www.2kblater.com/?p=8318
  * Description: Amazon Affiliate Store Plugin With Cart, Checkout, Custom Themes. Easy to manage and setup. Sell wide range of physical and digital products imported from Amazon Affiliate API using 90 days cookie reference. This plugin is released with GPL2 license.
- * Version: 1.0.1
+ * Version: 1.0.3
  * Author: 2kblater.com
  * Author URI: http://www.2kblater.com
  * License: GPL2
